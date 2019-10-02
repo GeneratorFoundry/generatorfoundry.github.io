893eaf418d86f2d8ea8507d1be18df74fc8c0510
A.W. Stanley <aws@sysfunc.net>
2019-10-03 09:46:13 +1000

    Manual pull from evil-generator.

---
+++ app/core/main.js
@@ -0,0 +1,33 @@
+define([
+	'core/foundry',
+	'lib/domReady',
+	'util/dom',
+	'gen/evil/basic-generator',
+	'gen/evil/data',
+	'gen/evil/titles',
+	'util/string'
+], function(foundry, domReady, domUtil, evilGenBasic, evilGenData, Title) {
+	function openPage(pageName, element) {
+		// Hide all elements with class="tabcontent" by default
+		let i, tabcontent, tablinks;
+		tabcontent = document.getElementsByClassName("tabcontent");
+		for (let i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+		}
+	
+		// Show the specific tab content
+		domUtil.byId(pageName).style.display = "block";
+		tablinks = document.getElementsByClassName("tablink");
+		for (let i = 0; i < tablinks.length; i++) {
+			tablinks[i].style.background = "";
+		}
+		element.style.background="linear-gradient(to bottom, #d0451b 5%, #bc3315 100%)"
+	}
+	window.openPage = openPage
+	
+	// Get the element with id="defaultOpen" and click on it
+	domUtil.byId("defaultOpen").click(); 
+	domReady(function(){
+		document.querySelector('#btn_generate').addEventListener('click', evilGenBasic.generate)
+	});
+});
